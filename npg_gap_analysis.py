779d1a7d93dc5f51a143dfd8b61ea94bd5c33d1b
harej <jamesmhare@gmail.com>
2015-10-29 17:50:36 -0400

    Undo

--- npg_gap_analysis.py
+++ npg_gap_analysis.py
@@ -52,7 +52,7 @@ def other_language_labels(blob, language_codes):
     output = {x: None for x in language_codes}  # Default is no label
     for entity in blob['entities']:
         for language_code in language_codes:
-            if language_code in entity['labels'].keys():
+            if language_code in entity['labels']:
                 output[language_code] = entity['labels'][language_code]['value']
     return output
 
